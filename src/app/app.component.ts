c6197979e48071516c4722c5a4b5444ecded476f
root <johnwinston1980@gmail.com>
2019-05-10 23:53:29 -0400

    index

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bajandanda';
+  title = 'bajandanda is finally working!';
 }
